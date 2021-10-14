d4ec1ed77c011f1c6ad30ed8fc9e53d23a99fc69
johanz97 <ejzambrano4@espe.edu.ec>
2021-10-14 15:40:04 -0500

    backend terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,25 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require('../controllers/socket');
 //Mensajes de sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
-    client.on('disconnect', () => { console.log('Cliente desconectado'); });
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token'])
+    //Verificar autenticación
+    if (!valido) { return client.disconnect(); }
+    //Cliente autenticado
+    usuarioConectado(uid);
+    //Unir usuario a una sala
+    client.join(uid);
+
+    //Escuchar mensaje-personal
+    client.on('mensaje-personal', async (payload) => {
+        //Grabar mensaje
+        await grabarMensaje(payload);
+        io.to(payload.para).emit('mensaje-personal', payload);
+    });
+
+    client.on('disconnect', () => { usuarioDesconectado(uid); });
     //Escuchar mensaje
     //client.on('mensaje', (payload) => {
     //    console.log('Mensaje', payload);
